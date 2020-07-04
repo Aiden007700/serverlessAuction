7379ab6dc44a64e233927300f4e360cdad8a935c
aiden <campbell.e.aiden@gmail.com>
2020-07-05 01:51:51 +0200

    aws yaml setup

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -1,8 +1,23 @@
+import { v4 as uuid } from "uuid";
+import AWS from "aws-sdk";
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
 async function createAuction(event, context) {
   const { title } = JSON.parse(event.body);
   const now = new Date();
 
-  const auction = { title, status: "OPEN", createdAt: now.toISOString() };
+  const auction = {
+    id: uuid(),
+    title,
+    status: "OPEN",
+    createdAt: now.toISOString(),
+  };
+
+  await dynamoDB.put({
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Item: auction,
+  }).promise();
 
   return {
     statusCode: 201,
